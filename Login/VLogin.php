0a213a084bef331a027f18a15613614804109bcb
JosephGarri <61102673+JosephGarri@users.noreply.github.com>
2020-03-14 14:58:39 -0600

    Update VLogin.php

--- Login/VLogin.php
+++ Login/VLogin.php
@@ -1,21 +1,31 @@
 <?php
-class VLogin{
-    protected $request;
-public function __construct($request)
+class VLogin
 {
-    $this->request = $request;
-}
-    public function verificar():bool{
-         $email=$_POST['email']??null;
-         $password=$_POST['pass']??null;
-        if($email=='joseph.rd09@gmail.com' && $password=='1234' ){
-           return  false;
-      
-          }else{
-             return true;
-             header('Location: index.php');
-          }
-  }
+    protected $request;
+    
+    public function __construct($request)
+    {
+        $this->request = $request;
     }
-?>
+    private function conexion():mysqli
+    {
+        $conn = mysqli_connect("database", "root", "", "DBTienda");
 
+        if (!$conn) {
+         return die("Connection failed: " . mysqli_connect_error());
+        }
+        return $conn;
+    }
+    public function verificar(): bool
+    {
+
+        $email = $_POST['email'] ?? null;
+        $password = $_POST['pass'] ?? null;
+        if ($email == 'joseph.rd09@gmail.com' && $password == '1234') {
+            return  false;
+        } else {
+            return true;
+            header('Location: index.php');
+        }
+    }
+}
