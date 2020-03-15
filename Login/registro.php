fc8d3bf36557553c35459b075e6c02670e07c271
JosephGarri <61102673+JosephGarri@users.noreply.github.com>
2020-03-15 11:32:27 -0600

    a

--- Login/registro.php
+++ Login/registro.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Login</title>
+    <title>Regist</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->
@@ -30,63 +30,66 @@
 </head>
 
 <body style="background-image: url(/Login/images/back1.jpg)">
-    <?php
-    $vincorrecto = false;
-    $redirect = true;
-    if ('POST' == $_SERVER['REQUEST_METHOD']) { //verifica el request
-        require '/clases/VLogin.php';       // se trae el archivo de la clase
-        $validar = new VLogin($_SERVER);   // instancia la clase del archivo php con el request
-        $vincorrecto = $validar->verificar();   //igualo lo que devuelve el metodo a una variable
-        $redirect = $validar->verificar();
-    }
 
-    ?>
+        <?php
+        $inserto = false;
+        $passIncorrecta = false;
+        if ('POST' == $_SERVER['REQUEST_METHOD']) {
+          require '/clases/registro.php';       
+          $registro = new clsRegistro($_SERVER);   
+          $inserto = $registro->ingresar_usuario();
+        
+        }
+
+        ?>
+    
+
+
     <div class="limiter">
         <div class="container-login100">
-            <div onclick="ocultar()" class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+            <div onclick="ocultar()" class="wrap-register100 p-l-55 p-r-55 p-t-65 p-b-54">
                 <form class="login100-form validate-form" method="POST">
                     <span class="login100-form-title p-b-49">
-                        Login
+                        Register form
                     </span>
 
-                    <div class="wrap-input100 validate-input m-b-23" data-validate="Username is required">
-                        <span class="label-input100">Email</span>
-                        <input class="input100" type="text" name="email" placeholder="Type your email">
-                        <span class="focus-input100" data-symbol="&#xf206;"></span>
-
+                    <div class="wrap-input100 validate-input" data-validate="Type yor name">
+                        <span class="label-input100">Name*</span>
+                        <input class="input100" type="text" name="name" placeholder="Type your name">
+                        <span class="focus-input100" data-symbol="&#128100;"></span>
                     </div>
-
+                    <div class="wrap-input100 validate-input m-b-23" data-validate="Email is required">
+                        <span class="label-input100">Email*</span>
+                        <input class="input100" type="email" name="name" placeholder="Type your email">
+                        <span class="focus-input100" data-symbol="&#64;"></span>
+                    </div>
+                  
                     <div class="wrap-input100 validate-input" data-validate="Password is required">
-                        <span class="label-input100">Password</span>
-                        <input class="input100" type="password" name="pass" placeholder="Type your password">
+                        <span class="label-input100">New Password*</span>
+                        <input class="input100" type="password" name="pass" placeholder="Type your new password">
+                        <span class="focus-input100" data-symbol="&#xf190;"></span>
+                    </div>
+                    <div class="wrap-input100 validate-input" data-validate="Password is required">
+                        <span class="label-input100">Conrfirm your Password</span>
+                        <input class="input100" type="password" name="pass" placeholder="Conrfirm your password please">
                         <span class="focus-input100" data-symbol="&#xf190;"></span>
                     </div>
 
                     <div class="text-right p-t-8 p-b-31">
 
                     </div>
-
-                    <?php if ($vincorrecto == true) : ?>
-                        <div style="color: black; background-color: #FF4141" id="alerta" name="alerta" class="alert alert-danger wrap-input100 validate-input m-b-23" role="alert">
-                            INCORRECT DATA
-                        </div>
-                    <?php endif; ?>
-
-                    <?php if ($redirect == false) {
-                        header('Location: /index/shop.php');
-                    } ?>
                     <div class="container-login100-form-btn">
                         <div class="wrap-login100-form-btn">
                             <div class="login100-form-bgbtn"></div>
 
                             <button type="submit" class="login100-form-btn" onclick="ocultar()">
-                                Login
+                                Register
                             </button>
                         </div>
                     </div>
                     <div>
                         <br>
-                        <div class="midiv"><a href="registro.php">register now</a></div>
+                
                     </div>
 
 
@@ -96,7 +99,7 @@
     </div>
 
 
-    <div id="dropDownSelect1"></div>
+  
     <script languague="javascript">
         function ocultar() {
 
